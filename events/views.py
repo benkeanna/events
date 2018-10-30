8fb52b72f30bc626b4cce1424b329e4fbde2f17c
Anna Haymannova <anna.haymannova@mergado.com>
2018-10-30 21:16:17 +0100

    Added events listing.

--- events/views.py
+++ events/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse('Hey, app running.')
+    html = '''
+    Hey, app running.
+    Check <a href="/events">offer</a>
+    '''
+    return HttpResponse(html)
+
+
+def event_listing(request):
+    html = '''
+    <ul>
+        <li>Chill on the beach</li>
+        <li>Camping in the woods</li>
+        <li>Flying into space</li>
+    </ul>
+    '''
+    return HttpResponse(html)
