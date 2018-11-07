b1b00ee7091dbb93ee3882083a78ccf3432a7dd1
Anna Haymannova <anna.haymannova@mergado.com>
2018-11-07 18:11:05 +0100

    Wrong structure.

--- events/models.py
+++ events/models.py
@@ -9,3 +9,6 @@ class Event(models.Model):
     description = models.TextField(max_length=1000)
     location = models.CharField(max_length=500)
     category = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
