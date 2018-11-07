d6d07388a1e3e322094cf105c828544e010b8f2a
Anna Haymannova <anna.haymannova@mergado.com>
2018-11-07 17:46:13 +0100

    New model Events, migration.

--- explorea/events/models.py
+++ explorea/events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    """
+    Basic event model.
+    """
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    location = models.CharField(max_length=500)
+    category = models.CharField(max_length=20)
