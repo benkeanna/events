566ec64c39227dc0290e2c844e6c0b5fcca33b90
Anna Haymannova <anna.haymannova@mergado.com>
2018-11-17 11:39:19 +0100

    Log in/out in base template.

--- explorea/accounts/views.py
+++ explorea/accounts/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def profile(request):
 
-    return render(request, 'accounts/profile.html', {'user': request.user})
+    return render(request, 'accounts/profile.html')
