af046f30455e06f7859b2d83779b4efb600aa88d
Anna Haymannova <anna.haymannova@mergado.com>
2018-11-17 15:11:21 +0100

    RegisterForm derived from ModelForm.

--- explorea/accounts/forms.py
+++ explorea/accounts/forms.py
@@ -1,12 +1,11 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 
-class RegisterForm(forms.Form):
-
-    username = forms.CharField(max_length=30, help_text='How will we call you?')
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
+class RegisterForm(forms.ModelForm):
+    """
+    Registration form derived from registration model.
+    """
     password1 = forms.CharField(
         label='Password',
         strip=False,
@@ -18,3 +17,12 @@ class RegisterForm(forms.Form):
         widget=forms.PasswordInput,
         help_text='Enter the same password as before, for verification.'
     )
+
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+        ]
