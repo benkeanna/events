6f32015091ec62b27c42780d2cb6eaac5a0cf085
Anna Haymannova <anna.haymannova@mergado.com>
2018-11-25 11:20:36 +0100

    Header.

--- explorea/accounts/urls.py
+++ explorea/accounts/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(
         next_page="/events/"), name='logout'),
     path('register/', views.register, name='register'),
-    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
