e9a1e9ebd266393d1f3e1b1db255cd11a5a20987
Anna Haymannova <anna.haymannova@mergado.com>
2018-12-09 16:56:42 +0100

    Password reset feature.

--- explorea/accounts/urls.py
+++ explorea/accounts/urls.py
@@ -1,16 +1,33 @@
 from django.urls import path
+from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
 from . import views
 
-
 urlpatterns = [
-    path('profile/', views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(
-        template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(
-        next_page="/events/"), name='logout'),
-    path('register/', views.register, name='register'),
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
-    path('change-password/', views.change_password, name='change_password'),
+	path('profile/', views.profile, name='profile'),
+	path('login/', auth_views.LoginView.as_view(
+			template_name='accounts/login.html'), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(
+			next_page="/events/"), name='logout'),
+	path('register/', views.register, name='register'),
+	path('profile/edit/', views.edit_profile, name='edit_profile'),
+	path('change-password/', views.change_password, name='change_password'),
+	path('reset-password/', auth_views.PasswordResetView.as_view(
+		template_name='accounts/reset_password.html', ),
+	     name='reset_password', ),
+	path('reset-password/done/', auth_views.PasswordResetDoneView.as_view(),
+	     name='password_reset_done'),
+	url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+	    auth_views.PasswordResetConfirmView.as_view(),
+	    name='password_reset_confirm',
+	    ),
+	path('reset-password/', auth_views.PasswordResetView.as_view(
+			template_name='accounts/reset_password.html',
+			email_template_name='accounts/reset_password_email.html'
+	),
+	     name='reset_password'),
+	path('reset-password/complete/',
+	     auth_views.PasswordResetCompleteView.as_view(),
+	     name='password_reset_complete'),
 ]
