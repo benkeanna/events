41167ed4555ea411d962e76d03c90e75dfe449ae
Anna Haymannova <anna.haymannova@mergado.com>
2018-12-09 15:10:25 +0100

    Added choices to event category.

--- explorea/events/models.py
+++ explorea/events/models.py
@@ -3,28 +3,44 @@ from django.conf.global_settings import AUTH_USER_MODEL
 
 
 class Event(models.Model):
-    """
-    Basic event model.
-    """
-    host = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+	"""
+	Basic event model.
+	"""
+	TRAVEL = 'TV'
+	RELAX = 'RX'
+	MUSIC = 'MC'
+	EDUCATION = 'EC'
 
-    name = models.CharField(max_length=200)
-    description = models.TextField(max_length=1000)
-    location = models.CharField(max_length=500)
-    category = models.CharField(max_length=20)
+	CATEGORY_CHOICES = (
+		(TRAVEL, 'travel'),
+		(RELAX, 'relax'),
+		(MUSIC, 'music'),
+		(EDUCATION, 'education'),
+	)
 
-    def __str__(self):
-        return self.name
+	host = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+	name = models.CharField(max_length=200)
+	description = models.TextField(max_length=1000)
+	location = models.CharField(max_length=500)
+	category = models.CharField(
+			max_length=20,
+			choices=CATEGORY_CHOICES,
+			default=RELAX,
+	)
+
+	def __str__(self):
+		return self.name
 
 
 class EventRun(models.Model):
-    """
-    Basic model for one event run.
-    """
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+	"""
+	Basic model for one event run.
+	"""
+	event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
-    date = models.DateField(blank=False, null=False)
-    time = models.TimeField(blank=False, null=False)
-    seats_available = models.PositiveIntegerField(blank=False, null=False)
-    price = models.DecimalField(
-        max_digits=10, decimal_places=2, blank=False, null=False)
+	date = models.DateField(blank=False, null=False)
+	time = models.TimeField(blank=False, null=False)
+	seats_available = models.PositiveIntegerField(blank=False, null=False)
+	price = models.DecimalField(
+			max_digits=10, decimal_places=2, blank=False, null=False)
